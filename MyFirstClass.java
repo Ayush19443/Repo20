fb2d354c652cf4b1dc78de1a3daca23ab5b7693c
Ayush Panwar <45512207+Ayush19443@users.noreply.github.com>
2024-07-31 22:21:56 +0530

    Update MyFirstClass.java

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -2,5 +2,6 @@ public class MyFirstClass {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello world");
+         System.out.println("hello how are you");
     }
 }
